96e546ad7ecaa83af60e76d996fceda3e239df87
Guzman D. Dario <guzman.d.dario@gmail.com>
2017-04-11 00:42:21 -0500

    update setup.sh for start service automatically

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ sudo cp pctracker_client.service /etc/systemd/system
 sudo mkdir -p /opt/pctracker_client
 sudo cp main.py /opt/pctracker_client
 sudo cp -r ./client /opt/pctracker_client
+sudo service pctracker_client start
